2142736e44e5430a82c16a3f0dc8cd0319f1c7ec
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-05 13:41:32 +0200

    Finishing app setup by cleaning up.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = Component = {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div></div>
+  };
 }
 
 export default App;
